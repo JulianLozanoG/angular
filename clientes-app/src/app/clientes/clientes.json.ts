d79afcfd810fad55e67774ab0e9359fb8a0d4dfc
Julian Lozano <jalozanog93@hotmail.com>
2019-11-04 22:31:37 -0500

    Se agregan directivas y enrutamiento de paginas

---
+++ clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,7 @@
+import { Cliente } from "./cliente";
+
+export const CLIENTES: Cliente[] = [
+    {id: 1,nombre: 'Julian', apellido: 'Lozano', email: 'jlozano@dcx.com', createAt: '02-11-2019'},
+    {id: 2,nombre: 'Juan', apellido: 'Lopez', email: 'jlopez@dcx.com', createAt: '02-11-2019'},
+    {id: 3,nombre: 'Henry', apellido: 'Hernandez', email: 'hhernandez@dcx.com', createAt: '02-11-2019'}
+  ];
