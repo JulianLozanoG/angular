a451f4c495fd7cef8206072bed60678f79da8cc2
Julian Lozano <jalozanog93@hotmail.com>
2019-11-16 22:23:29 -0500

    Se suben cambios para el manejo de validaciones bk

--- clientes-app/src/app/clientes/cliente.service.ts
+++ clientes-app/src/app/clientes/cliente.service.ts
@@ -27,6 +27,10 @@ export class ClienteService {
     return this.http.post(this.urlEndpoint, cliente, {headers: this.httpHeaders}).pipe(
       map((response: any) => response.cliente as Cliente),
       catchError(e => {
+
+        if(e.status==400){
+          return throwError(e);
+        }
         console.error(e.error.mensaje);
         Swal.fire(e.error.mensaje, e.error.error, 'error');
         return throwError(e);
@@ -48,6 +52,10 @@ export class ClienteService {
   update(cliente: Cliente): Observable<any>{
     return this.http.put<any>(`${this.urlEndpoint}/${cliente.id}`, cliente, {headers: this.httpHeaders}).pipe(
       catchError(e => {
+
+        if(e.status==400){
+          return throwError(e);
+        }
         console.error(e.error.mensaje);
         Swal.fire(e.error.mensaje, e.error.error, 'error');
         return throwError(e);
--- clientes-app/src/app/clientes/form.component.ts
+++ clientes-app/src/app/clientes/form.component.ts
@@ -12,6 +12,7 @@ export class FormComponent implements OnInit {
 
   private cliente: Cliente = new Cliente()
   private titulo: string = "Crear Cliente"
+  private errores: string[];
 
   constructor(private clienteService: ClienteService,
   private router: Router,
@@ -35,6 +36,11 @@ export class FormComponent implements OnInit {
     .subscribe(cliente => {
       this.router.navigate(['/clientes'])
       Swal.fire('Nuevo cliente',`El cliente: ${cliente.nombre} ha sido creado con éxito`, 'success')
+    },
+    err => {
+      this.errores = err.error.errors as string[];
+      console.error("Código de error desde el backend: "+err.status);
+      console.error(err.error.errors);
     }
     )
   }
@@ -44,6 +50,11 @@ export class FormComponent implements OnInit {
     .subscribe( json => {
       this.router.navigate(['/clientes'])
       Swal.fire('Cliente actualizado',`${json.mensaje}: ${json.cliente.nombre}`, 'success')
+    },
+    err => {
+      this.errores = err.error.errors as string[];
+      console.error("Código de error desde el backend: "+err.status);
+      console.error(err.error.errors);
     }
     )
   }
